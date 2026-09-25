6f6e78ab6d2f580579881e76e37f70bf4a16917f
jose-espinoza-37 <jose1874pw@hotmail.com>
2026-09-24 20:09:20 -0500

    Crear sub divisiones para que esten en orden cada una de las pestañas

---
+++ Frontend/Nautica/src/app/components/home/contacto/contacto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-contacto',
+  styleUrl: './contacto.scss',
+  templateUrl: './contacto.html',
+})
+export class Contacto {}
---
+++ Frontend/Nautica/src/app/components/home/info/info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-info',
+  styleUrl: './info.scss',
+  templateUrl: './info.html',
+})
+export class Info {}
