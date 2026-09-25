84b4522cf0d4751162b7ec45b873e5773c05a0ae
jose-espinoza-37 <jose1874pw@hotmail.com>
2026-09-25 04:58:44 -0500

    botón dentro de container- desarrollo de footer

--- Frontend/Nautica/src/app/components/footer/footer.ts
+++ Frontend/Nautica/src/app/components/footer/footer.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'app-footer',
+  standalone: true,
   styleUrl: './footer.scss',
   templateUrl: './footer.html',
 })
